6afdb558b412952ab62d8d2c71393d9e90df0057
fangdawei <fangdawei_www@163.com>
2019-04-21 13:36:25 +0800

    delete log

--- datawatcher-plugin/src/main/groovy/club/fdawei/datawatcher/plugin/DataWatcherPlugin.groovy
+++ datawatcher-plugin/src/main/groovy/club/fdawei/datawatcher/plugin/DataWatcherPlugin.groovy
@@ -1,6 +1,5 @@
 package club.fdawei.datawatcher.plugin
 
-import club.fdawei.datawatcher.plugin.log.PluginLogger
 import com.android.build.gradle.AppExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
@@ -10,7 +9,6 @@ class DataWatcherPlugin implements Plugin<Project> {
     private static final String TAG = "DataWatcherPlugin"
 
     void apply(Project project) {
-        PluginLogger.i(TAG, "apply DataWatcherTransform")
         def android = project.extensions.findByType(AppExtension)
         android.registerTransform(new DataWatcherTransform())
     }
